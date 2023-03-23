8f94cf39cb04448a847f15adccc123bdd2f9cf34
eliasjp <illumine1998@gmail.com>
2023-03-23 13:19:12 -0300

    feat: add product function

--- management/product_handler.py
+++ management/product_handler.py
@@ -1,6 +1,6 @@
 from menu import products
 
-def get_product_by_id(id):
+def get_product_by_id (id):
     for x in products:
         if (x["_id"] == id):
             return x
@@ -8,7 +8,7 @@ def get_product_by_id(id):
     return {}
 
         
-def get_products_by_type(type):
+def get_products_by_type (type):
     filtered = []
 
     for x in products:
@@ -16,3 +16,21 @@ def get_products_by_type(type):
             filtered.append(x)
         
     return filtered
+
+def add_product (product_list: list, **dictItem: dict) -> dict:
+    index = 1
+
+    if len(product_list) == 0:
+        dictItem.update({ "_id": index })
+        product_list.append(dictItem)
+        return dictItem
+
+    for item in product_list:
+        if item["_id"] >= index:
+            index = item["_id"] + 1
+
+
+    dictItem.update({ "_id": index })
+    product_list.append(dictItem)
+            
+    return dictItem
