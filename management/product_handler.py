ca666ccbe70f9dfe08303f42976a4dc66ee434e4
eliasjp <illumine1998@gmail.com>
2023-03-24 15:49:45 -0300

    feat: extra add product function

--- management/product_handler.py
+++ management/product_handler.py
@@ -60,3 +60,33 @@ def menu_report ():
     avg_price = sum_price / count
 
     return f"Products Count: {count} - Average Price: ${round(avg_price, 2)} - Most Common Type: {most_item_position}"
+
+def add_product_extra (menu: list, *required: tuple, **new_product: dict) -> dict:
+    dict_keys = list(new_product.keys())
+    tuple_keys = list(required)
+
+    for tuple_key in tuple_keys:
+        if tuple_key not in dict_keys:
+            raise(KeyError(f"field {tuple_key} is required"))
+        
+    for dict_key in dict_keys:
+        if dict_key not in tuple_keys:
+            new_product.pop(dict_key)
+
+    index = 1
+
+    if len(menu) == 0:
+        new_product.update({ "_id": index })
+        menu.append(new_product)
+        return new_product
+
+    for item in menu:
+        if item["_id"] >= index:
+            index = item["_id"] + 1
+
+
+    new_product.update({ "_id": index })
+    menu.append(new_product)
+    
+
+    return new_product
