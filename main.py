d0643a36469939cbada564b799f143661a9ad71e
eliasjp <illumine1998@gmail.com>
2023-03-24 15:05:19 -0300

    feat: menu report function

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-# from management.product_handler import get_product_by_id, get_products_by_type, add_product
-from management.tab_handler import calculate_tab
+from management.product_handler import  menu_report
+# from management.tab_handler import calculate_tab
 
 if __name__ == "__main__":
     # print(get_product_by_id(21))
     # print(get_products_by_type("drink"))
-    # print(add_product(list, new_product))
-    print(calculate_tab([
-                    {"_id": 10, "amount": 3},
-                    {"_id": 20, "amount": 2},
-                    {"_id": 21, "amount": 5},
-                ]))
+    # print(add_product())
+    # print(calculate_tab())
+    print(menu_report())
     ...
